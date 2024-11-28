c135a11e4607b08d8905efb9bf4345c14179413b
j1eo <jleojimenezg12@gmail.com>
2024-11-27 18:25:08 -0600

    Conexion a DB

--- src/dataBase/ConexionDataBase.java
+++ src/dataBase/ConexionDataBase.java
@@ -1,52 +1,49 @@
 package dataBase;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDataBase {
 
-	 private String url;
-	    private String username;
-	    private String pwd;
-	    private Connection connection;
+    private String url;
+    private String username;
+    private String pwd;
+    private Connection connection;
 
-	    // Constructor
-	    public ConexionDataBase(String url, String username, String pwd) {
-	        this.url = url;
-	        this.username = username;
-	        this.pwd = pwd;
-	    }
+    // Constructor
+    public ConexionDataBase(String url, String username, String pwd) {
+        this.url = url;
+        this.username = username;
+        this.pwd = pwd;
+    }
 
-	    // Abrir conexión
-	    public void openConnection() throws SQLException {
-	        try {
-	            if (this.connection != null && !this.connection.isClosed()) {
-	                // La conexión ya está abierta
-	                return;
-	            }
-	            // Registrar el driver de MySQL
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	            // Establecer la conexión
-	            this.connection = DriverManager.getConnection(url, username, pwd);
-	            System.out.println("TODO BIEN");
-	        } catch (ClassNotFoundException e) {
-	            throw new SQLException("Error al cargar el driver de MySQL", e);
-	        }
-	    }
+    // Abrir conexión
+    public void openConnection() throws SQLException {
+        try {
+            if (this.connection != null && !this.connection.isClosed()) {
+                return;
+            }
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(url, username, pwd);
+            System.out.println("Conexión abierta con éxito.");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Error al cargar el driver de MySQL", e);
+        }
+    }
 
-	    // Cerrar conexión
-	    public void closeConnection() throws SQLException {
-	        if (this.connection != null && !this.connection.isClosed()) {
-	            this.connection.close();
-	        }
-	    }
+    // Cerrar conexión
+    public void closeConnection() throws SQLException {
+        if (this.connection != null && !this.connection.isClosed()) {
+            this.connection.close();
+        }
+    }
 
-	    // Getter para obtener la conexión
-	    public Connection getConnection() throws SQLException {
-	        if (this.connection == null || this.connection.isClosed()) {
-	            openConnection();
-	        }
-	        return this.connection;
-	    }
-
+    // Getter para obtener la conexión
+    public Connection getConnection() throws SQLException {
+        if (this.connection == null || this.connection.isClosed()) {
+            openConnection();
+        }
+        return this.connection;
+    }
 }
