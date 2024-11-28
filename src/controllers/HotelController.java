c059fe6983898a8a9019b39c6b6090e647ea3554
j1eo <jleojimenezg12@gmail.com>
2024-11-27 18:22:44 -0600

    Controller adaptado a DB

--- src/controllers/HotelController.java
+++ src/controllers/HotelController.java
@@ -2,47 +2,61 @@ package controllers;
 
 import beans.HabitacionBean;
 import beans.HotelBean;
+import dataBase.ConexionDataBase;
 import services.HotelService;
 
+import javax.swing.JOptionPane;
 import java.util.ArrayList;
 
 public class HotelController {
 
     private HotelService hotelService;
 
-    public HotelController() {
-        this.hotelService = new HotelService();
+    public HotelController(ConexionDataBase conexionDataBase) {
+        this.hotelService = new HotelService(conexionDataBase);
     }
 
-    public void agregarHotel(String nombre, int estrellas) {
-        hotelService.agregarHotel(nombre, estrellas);
+    public void agregarHotel() {
+        String nombre = JOptionPane.showInputDialog("Ingresa el nombre del Hotel: ");
+        String estrellasStr = JOptionPane.showInputDialog("Ingresa la cantidad de Estrellas: ");
+        int estrellas = Integer.parseInt(estrellasStr);
+        String direccion = JOptionPane.showInputDialog("Ingresa la dirección del Hotel: ");
+        String telefono = JOptionPane.showInputDialog("Ingresa el teléfono del Hotel: ");
+        
+        hotelService.agregarHotel(nombre, estrellas, direccion, telefono);
+        JOptionPane.showMessageDialog(null, "Hotel " + nombre + " con " + estrellas + " estrellas agregado exitosamente.");
     }
 
-    public ArrayList<HotelBean> listarHoteles() {
-        return hotelService.listarHoteles();
-    }
-    public HotelBean buscarHotelPorNombre(String Nombre) {
-    	return hotelService.buscarHotelPorNombre(Nombre);
+    public void listarHoteles() {
+        ArrayList<HotelBean> hoteles = hotelService.listarHoteles();
+        StringBuilder sb = new StringBuilder("Hoteles registrados:\n");
+        for (HotelBean hotel : hoteles) {
+            sb.append("Nombre: ").append(hotel.getNombre())
+              .append(", Estrellas: ").append(hotel.getEstrellas())
+              .append(", Dirección: ").append(hotel.getDireccion())
+              .append(", Teléfono: ").append(hotel.getTelefono())
+              .append("\n");
+        }
+        JOptionPane.showMessageDialog(null, sb.toString());
     }
 
-    public void agregarHabitacion(String nombreHotel, String ID, String tipo) {
-        HabitacionBean habitacion = new HabitacionBean(ID, tipo);
+    public void agregarHabitacion() {
+        String nombreHotel = JOptionPane.showInputDialog("Ingresa el nombre del Hotel al que deseas agregar una habitación: ");
+        String id = JOptionPane.showInputDialog("Ingresa el ID de la habitación: ");
+        String tipo = JOptionPane.showInputDialog("Ingresa el tipo de habitación: ");
+        
+        HabitacionBean habitacion = new HabitacionBean(id, tipo);
         hotelService.agregarHabitacion(nombreHotel, habitacion);
+        JOptionPane.showMessageDialog(null, "Habitación " + id + " agregada exitosamente al hotel " + nombreHotel + ".");
     }
 
-    public ArrayList<HabitacionBean> listarHabitacionesDeHotel(String nombreHotel) {
-        return hotelService.listarHabitacionesDeHotel(nombreHotel);
-    }
-
-    public double calcularCostoPorDia(String nombreHotel, String habitacionID) {
-        return hotelService.calcularCostoPorDia(nombreHotel, habitacionID);
-    }
-
-    public double calcularComision(String nombreHotel, String habitacionID) {
-        return hotelService.calcularComision(nombreHotel, habitacionID);
-    }
-
-    public HabitacionBean buscarHabitacionPorID(String nombreHotel, String habitacionID) {
-        return hotelService.buscarHabitacionPorID(nombreHotel, habitacionID);
+    public void listarHabitacionesDeHotel() {
+        String nombreHotel = JOptionPane.showInputDialog("Ingresa el nombre del Hotel para listar sus habitaciones: ");
+        ArrayList<HabitacionBean> habitaciones = hotelService.listarHabitacionesDeHotel(nombreHotel);
+        StringBuilder sb = new StringBuilder("Habitaciones del hotel " + nombreHotel + ":\n");
+        for (HabitacionBean habitacion : habitaciones) {
+            sb.append("ID: ").append(habitacion.getID()).append(", Tipo: ").append(habitacion.getTipo()).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, sb.toString());
     }
 }
