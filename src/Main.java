915db846fe74a165aa1287eba13992555447b74b
j1eo <jleojimenezg12@gmail.com>
2024-12-07 18:46:24 -0600

    Agregados nuevos controladores y servicios

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import controllers.HotelController;
 import controllers.EmpleadoController;
+import controllers.ReservacionController;
 import controllers.SystemController;
 import dataBase.ConexionDataBase;
 import dataBase.InicializarTablas;
 import dataBase.LoadConfig;
 import services.HotelService;
 import services.EmpleadoService;
+import services.ReservacionService;
+import services.VendedorService;
 import views.HotelViews;
 import views.EmpleadoViews;
+import views.ReservacionViews;
 import views.SystemViews;
 
 import java.sql.Connection;
@@ -43,18 +47,32 @@ public class Main {
             // Crear servicios
             HotelService hotelService = new HotelService(conexionDataBase);
             EmpleadoService empleadoService = new EmpleadoService(conexionDataBase);
+            ReservacionService reservacionService = new ReservacionService(conexionDataBase);
+            VendedorService vendedorService = new VendedorService(conexionDataBase);
 
-            // Crear controladores y vistas
+            // Verificar y actualizar reservaciones expiradas al iniciar el programa
+            try {
+                reservacionService.verificarYActualizarReservacionesExpiradas();
+            } catch (SQLException e) {
+                System.err.println("Error al verificar y actualizar reservaciones expiradas: " + e.getMessage());
+            }
+
+            // Crear vistas
             EmpleadoViews empleadoViews = new EmpleadoViews();
-            EmpleadoController empleadoController = new EmpleadoController(empleadoService, empleadoViews);
-            HotelViews hotelViews = new HotelViews(null); 
-            HotelController hotelController = new HotelController(hotelService, hotelViews, empleadoController, empleadoViews);
-           
-            SystemController systemController = new SystemController(hotelViews, hotelController, empleadoViews, empleadoController);
+            SystemViews systemViews = new SystemViews();
+            ReservacionViews reservacionViews = new ReservacionViews();
 
-            // Mostrar el menú principal del sistema
-            SystemViews systemViews = new SystemViews(systemController);
-            systemViews.mostrarMenuPrincipal();
+            // Crear controladores
+            EmpleadoController empleadoController = new EmpleadoController(empleadoService, empleadoViews, hotelService);
+            HotelViews hotelViews = new HotelViews(null);
+            HotelController hotelController = new HotelController(hotelService, hotelViews, empleadoController, empleadoViews);
+            ReservacionController reservacionController = new ReservacionController(reservacionService, vendedorService, hotelService, reservacionViews);
+
+            // Crear SystemController con todos los controladores y vistas
+            SystemController systemController = new SystemController(hotelViews, hotelController, empleadoViews, empleadoController, systemViews, reservacionController, reservacionViews, reservacionService, hotelService);
+
+            // Mostrar el menú principal
+            systemViews.mostrarMenuPrincipal(systemController);
 
         } catch (SQLException e) {
             System.err.println("Error en la operación SQL: " + e.getMessage());
